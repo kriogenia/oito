50e77e9cb0b94233565b71b930d8ef96cc310c53
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-04-24 23:34:38 +0200

    Opcode fetching

--- oito_core/src/cpu.rs
+++ oito_core/src/cpu.rs
@@ -6,8 +6,9 @@ use register::{VRegister, IRegister};
 use stack::Stack;
 use timer::Timer;
 
-use crate::Address;
+use crate::{Address, OpCode, exception::Exception, Byte};
 
+const INSTRUCTION_SIZE: u16 = 2;
 const NUMBER_OF_REGISTERS: usize = 16;
 const STARTING_ADDRESS: Address = 0x200;
 
@@ -27,6 +28,34 @@ pub struct Cpu {
 	st: Timer,
 }
 
+impl Cpu {
+
+	/// Performs a tick of the CPU
+	pub fn tick<M>(&mut self, memory_access: M) 
+	where M: Fn(Address) -> Result<Byte, Exception> {
+		let _opcode = self.fetch(self.pc, memory_access);
+		// Decode instruction
+		// Execute instruction
+	}
+
+	/// Reads from memory the next instruction and points to the next one
+	fn fetch<M>(&mut self, address: Address, read: M) -> Result<OpCode, Exception> 
+	where M: Fn(Address) -> Result<Byte, Exception> {
+		let big_byte = read(address)? as u16;
+		let small_byte = read(address + 1)? as u16;
+		let opcode = (big_byte << 8) | small_byte;
+		
+		self.increase();
+		Ok(opcode)
+	}
+
+	/// Increases the Program Counter to point to the next instruction
+	fn increase(&mut self) {
+		self.pc += INSTRUCTION_SIZE;
+	}
+
+}
+
 impl Default for Cpu {
     fn default() -> Self {
         Self { 
@@ -38,4 +67,30 @@ impl Default for Cpu {
 			st: Default::default() 
 		}
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{ STARTING_ADDRESS, Cpu };
+
+
+	#[test]
+	fn fetch() {
+		let mut cpu = Cpu::default();
+		let opcode = cpu.fetch(1, |address| { Ok(address as u8) });
+		assert_eq!(0x0102, opcode.unwrap());
+		assert_eq!(STARTING_ADDRESS + 2, cpu.pc);
+	}
+
+	#[test]
+	fn increase() {
+		let mut cpu = Cpu::default();
+		assert_eq!(STARTING_ADDRESS, cpu.pc);
+		for i in 1..4 {
+			cpu.increase();
+			assert_eq!(i * 2 + STARTING_ADDRESS, cpu.pc);
+		}
+	}
+
+
 }
--- oito_core/src/lib.rs
+++ oito_core/src/lib.rs
@@ -10,8 +10,8 @@ use vram::VRam;
 
 /// Specification of the address type to correctly indicate when it's being used
 pub type Address = u16;
-/// Specification of the instruction type to correctly indicate when it's being used
-pub type Instruction = u16;		// each Chip8 instruction is made of two bytes
+/// Specification of the opcode type to correctly indicate when it's being used
+pub type OpCode = u16;		// each Chip8 instruction is made of two bytes
 /// Specification of the bit type to correctly indicate what it's built upon bits
 pub type Byte = u8;
 
