b2d44ffe1a5ab3bce76ef6b5ddc3b7392cd11fdf
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-05-02 20:36:33 +0200

    Add core API

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,29 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+use oito_core::core::OitoCore;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub struct OitoWasm {
+	oito: OitoCore
+}
+
+#[wasm_bindgen]
+impl OitoWasm {
+	
+	#[wasm_bindgen(constructor)]
+	pub fn new() -> Self {
+		Self {
+			oito: OitoCore::new()
+		}
+	}
+	
+	#[wasm_bindgen]
+	pub fn tick(&mut self) {
+		self.oito.tick().unwrap();
+	}
+	
+	#[wasm_bindgen]
+	pub fn frame_tick(&mut self) {
+		self.oito.frame_tick();
+	}
+
 }
